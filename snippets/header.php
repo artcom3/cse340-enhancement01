a702656059c35f1c186b74c58057363aac1da918
Kevin Haro <artcom_3@hotmail.com>
2021-05-11 20:44:23 -0500

    Enhancement 2
    Add PDO Connection

--- snippets/header.php
+++ snippets/header.php
@@ -1,4 +1,4 @@
 <div id="top-header">
-    <img src="./images/site/logo.png" alt="PHP Motors logo" id="logo">
-    <a href="./accounts?action=login-page" title="Login or Register with PHP Motors" id="acc">My Account</a>
+    <img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo" id="logo">
+    <a href="/accounts?action=login-page" title="Login or Register with PHP Motors" id="acc">My Account</a>
 </div>
--- snippets/nav.php
+++ snippets/nav.php
@@ -1,5 +1,5 @@
 <ul>
-    <li><a href="../phpmotors/" title="PHP Motors home page">Home</a></li>
+    <li><a href="/phpmotors/" title="PHP Motors home page">Home</a></li>
     <li><a href="#" title="clasic cars page">Classic</a></li>
     <li><a href="#" title="sports cars">Sports</a></li>
     <li><a href="#" title="sports utility vehicles">SUV</a></li>
